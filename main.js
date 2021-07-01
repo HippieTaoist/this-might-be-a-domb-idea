cf74c5a879a39a321cfa3c997444a511ff83d6a6
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-07-01 12:52:54 -0600

    Add | Test | Complete Code | With a little Extra

---
+++ main.js
@@ -0,0 +1,17 @@
+const p = document.querySelector('p')
+
+p.style.color = 'lightblue';
+
+p.style.fontSize = '48px'
+let arrOfWords = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+p.innerHTML = arrOfWords
+document.getElementById("item-13").style.opacity = '0.5';
+
+document.querySelector('img').src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+
+document.querySelector('img').style.height = '300px'
+
+document.getElementById('myImage').style.height = '300px'
+
+document.getElementById('item-16').innerText = "Won't Get Fooled Again!!"
